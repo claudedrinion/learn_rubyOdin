3f581eef768c3f9ed4d61c0f3f6e63a50b75b5bb
claudedrinion <claude.drinion@gmail.com>
2017-02-26 16:39:19 -0500

    Add more custom Enumerable methods

--- enumerable.rb
+++ enumerable.rb
@@ -18,8 +18,47 @@ module Enumerable
       result << element if yield(element) == true
     end
   end
+
+	def my_all?
+		all = true
+		self.my_each do |element|
+			if yield(element) == false
+				return all = false
+			end
+		end
+		all
+	end
+
+	def my_none?
+		none = true
+		self.my_each do |element|
+			if yield(element) == true
+				return none = false
+			end
+		end
+		none
+	end
+	
+	def my_count
+		count = 0
+		self.my_each do |element|
+			count += 1 if yield(element)
+		end
+		count
+	end
+
 end
+
 
 test = [1,2,3,4]
 test.my_each { |n| puts n * 2 }
+puts(test.my_all? do |element| element < 5 end)
+puts(test.my_all? do |element| element < 2 end)
+ary = [2,3,4,3,2,5,3]
+puts(ary.count { |x| x % 2 == 0 })
+
+
+
+
+
 
